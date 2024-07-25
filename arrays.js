2590124d977f1d36b7cd0a89bd8959c9502a47d5
Muhammad Ghani <muhammadghaniabid@gmail.com>
2024-07-25 01:31:10 -0700

    Mern 19 7/25/2024 I just added js file

---
+++ arrays.js
@@ -0,0 +1,25 @@
+// const cars = [
+//     "Saab",
+//     "Volvo",
+//     "BMW",
+//     [1,2,3],
+//     {
+//         age : 20,
+//         height : 30
+//     }
+//   ];
+//   console.log(cars[4].age);
+//   cars.push("Bently");
+//   console.log(cars);
+//   cars.unshift({
+//     Model: 2012,
+//     size: 12,
+//     condition: "old",
+//     color:"red"
+    
+//   });
+//   console.log(cars);
+//   cars.pop();
+//   console.log(cars);
+//   cars.indexOf("Volvo");
+//   console.log(cars);
